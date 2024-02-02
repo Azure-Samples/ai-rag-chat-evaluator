09a9ddfe1af38db62cdb37aabf4c4e7fc01e2c05
Pamela Fox <pamela.fox@gmail.com>
2024-02-02 22:35:01 +0000

    Fix numquestions correctly

--- scripts/cli.py
+++ scripts/cli.py
@@ -17,12 +17,19 @@ logging.basicConfig(
 dotenv.load_dotenv()
 
 
+def int_or_none(raw: str) -> int | None:
+    return None if raw == "None" else int(raw)
+
+
 @app.command()
 def evaluate(
     config: Path = typer.Option(
         exists=True, dir_okay=False, file_okay=True, help="Path to config.json", default="config.json"
     ),
-    numquestions: int | None = typer.Argument(help="Number of questions to evaluate", default=None),
+    numquestions: int
+    | None = typer.Option(
+        help="Number of questions to evaluate (defaults to all if not specified).", default=None, parser=int_or_none
+    ),
 ):
     run_evaluate_from_config(Path.cwd(), config, numquestions)
 
